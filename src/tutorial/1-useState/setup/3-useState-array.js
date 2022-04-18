85b7d01ee38198af793e5009942817f020bd4e69
91639 <goel.aryan2406@gmail.com>
2022-04-18 12:27:40 +0530

    useState in arrays

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,13 +3,30 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+  const removeItems = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    //?filtering the array based on id
+    //? if the id we passed is not equal to the array elements id we return it
+    setPeople(newPeople);
+  };
 
   return (
     <>
       {people.map((person) => {
-        console.log(person);
-        return "hello";
+        const { id, name } = person;
+
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItems(id)}>
+              clear
+            </button>
+          </div>
+        );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        remove
+      </button>
     </>
   );
 };
